9e4abbb1ecb8773e900ad6531ee1b3ada9da9ae6
Vanessa Ho <poyiu.ho@stonybrook.edu>
2017-08-05 19:04:27 -0400

    Added a few animations to previous sections such as the image

--- javascript/common.js
+++ javascript/common.js
@@ -1,9 +1,11 @@
+// function for lowercasing the first letter of a string
 function lowercase(string) {
     return string.substr(0,1).toLowerCase() + string.substr(1);
 }
 
 
 $(document).ready(function() {
+    //variables
     var $find = $("#find");
     var $owl = $(".owl");
     var $about = $("#about");
@@ -14,10 +16,12 @@ $(document).ready(function() {
     var $section = $(".section");
     var $navbar = $("#navbar");
     
+    // so that if the window refreshes in middle of page the user isn't stuck there
     if ($(window).scrollTop() >= $section.height() - $navbar.height()) {
         $('html, body').css("overflow-y", "auto");
     };
     
+    // click on whatever link on navigation bar
     $navp.click(function() {
         var $offset = "#" + lowercase($(this).text());
         $('html, body').animate({  
@@ -25,12 +29,14 @@ $(document).ready(function() {
         }, 500);
     });
     
+    // click on "Find out more about me"
     $find.click(function() {
         $('html, body').animate({
             scrollTop: $about.offset().top
         }, 500);
     });
     
+    // click on owl icon on top
     $owl.click(function() {
         var $opening = $("#opening");
         $('html, body').animate({
@@ -38,9 +44,13 @@ $(document).ready(function() {
         }, 500);
     });
     
+    // scrolling functions
     $(window).scroll(function() {
         var $nav = $(".navp");
         var $owl = $(".owl img");
+        var $me = $("#aleft img");
+        
+        // if the window has passed the top section's height minus navbar's height
         if ($(this).scrollTop() > $section.height() - $navbar.height()) {
             $('html, body').css("overflow-y", "auto");
             $(".owl").addClass("opacity");
@@ -54,15 +64,21 @@ $(document).ready(function() {
             $(".owl img").attr("src", "./img/owl.png");
         }
         
+        // fade each element in one by one
         $wrapper.each(function(i) {
             // middle of window
             var window_pos = $(window).scrollTop() + ($(window).height() / 2);
             // top of element
             var top_wrapper = $(this).offset().top;
-
+            
             if (top_wrapper <= window_pos) {
                 $(this).animate({'opacity':'1'}, 1000*i);
             }
         });
+        
+        // rotate image after scrolling past #about section
+        if ($(this).scrollTop() >= $about.offset().top - $navbar.height()) {
+            $me.addClass("rotate");
+        }
     });
 });
