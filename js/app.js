a5df35a3b71ac1633cbda090fe738e371b3d702c
Vanessa Ho <poyiu.ho@stonybrook.edu>
2018-03-15 17:54:06 -0400

    rearranging loading for linking files

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 // forced scroll to top
-$(document).ready(function() {
-    $(this).scrollTop(0);
-});
+window.onbeforeunload = function() {window.scrollTo(0,0);}
 
 //typed.js option
 var options = {
